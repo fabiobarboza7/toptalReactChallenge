564dc431b32fcbe5f603bf2bc0702927163e77e4
Fábio Souza <fabiobarboza7@hotmail.com>
2020-02-06 15:23:18 -0300

    starting oposity feature

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,18 +2,15 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    jest: true
   },
   extends: [
+    'plugin:react/recommended',
     'airbnb',
-    'prettier',
-    'prettier/react'
   ],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
-
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -23,40 +20,7 @@ module.exports = {
   },
   plugins: [
     'react',
-    'prettier',
-    'react-hooks',
-    'import',
-    'import-helpers'
   ],
   rules: {
-    'prettier/prettier': 'error',
-    'react/jsx-filename-extension': [
-      'warn',
-      {extensions: ['.jsx', '.js']}
-    ],
-    'import/prefer-default-export': 'off',
-    'no-underscore-dangle': 'off',
-    'no-param-reassign': 'off',
-    'react/prop-types': 'off',
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-    'import/no-unresolved': 'off',
-    'no-console': 'off',
-    'camelcase': 'off',
-    'react/jsx-props-no-spreading': "off",
-    'import-helpers/order-imports': [
-      'warn',
-      {
-        'newlinesBetween': 'always',
-        'groups': [
-          '/^react/',
-          'module',
-          '/^~/',
-          '/^@shared/',
-          ['parent', 'sibling', 'index'],
-        ],
-        'alphabetize': { 'order': 'asc', 'ignoreCase': true }
-      }
-    ]
-  }
+  },
 };
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,33 +1,34 @@
 import React, { useState } from 'react';
 
-import { SquareContainer, Square, Icon } from './styles.js'
+import { SquareContainer, Square, Icon } from './styles.js';
 
 export default function Home() {
   const [squares, setSquares] = useState([
-    {id: 1, isActive: false},
-    {id: 2, isActive: false},
-    {id: 3, isActive: false},
-    {id: 4, isActive: false},
-    {id: 5, isActive: false},
-    {id: 6, isActive: false},
-    {id: 7, isActive: false},
-    {id: 8, isActive: false},
-    {id: 9, isActive: false},
+    { id: 1, isActive: false },
+    { id: 2, isActive: false },
+    { id: 3, isActive: false },
+    { id: 4, isActive: false },
+    { id: 5, isActive: false },
+    { id: 6, isActive: false },
+    { id: 7, isActive: false },
+    { id: 8, isActive: false },
+    { id: 9, isActive: false },
   ]);
 
+  const [oposity, setOposity] = useState(false);
+
   function handleAddIcon(square) {
-    square.isActive = !square.isActive;
-    setSquares([...squares])
+    square.isActive = true;
+    setSquares([...squares]);
   }
 
   return (
     <SquareContainer>
-      {console.log(squares)}
-      {squares.map(_square => (
+      {squares.map((_square) => (
         <Square key={_square.id} onClick={() => handleAddIcon(_square)}>
           {_square.isActive && <Icon>×</Icon>}
         </Square>
       ))}
     </SquareContainer>
-  )
+  );
 }
